77d0c2d4f66bcd809945e41114db54a39caf9ebf
Niang Mohamed Lamine <106320478+lamine2002@users.noreply.github.com>
2024-04-30 14:52:20 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    if (q.pathname == "/") {
+        q.pathname = "/index";
+    }
+    var filename = "." + q.pathname + ".html";
+    fs.readFile(filename, function(err, data) {
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        fs.readFile('404.html', function(err, data){
+            if (err) {
+                res.write("404 Not Found");
+            } else {
+                res.write(data);
+            }
+            return res.end();
+        });
+      } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+      }
+    });
+}).listen(8080);
